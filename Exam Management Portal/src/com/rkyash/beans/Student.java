7379182edf6d717d1d1456b5d6917745159d644f
rkyash <35574207+rkyash@users.noreply.github.com>
2018-06-23 00:51:08 +0530

    Student.java
    Student.java

--- Exam Management Portal/src/com/rkyash/beans/Student.java
+++ Exam Management Portal/src/com/rkyash/beans/Student.java
@@ -1,5 +1,27 @@
 package com.rkyash.beans;
 
 public class Student {
+	
+	private long studentId;
+	private String studentFirstName;
+	private String studentLastName;
+	public long getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(long studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentFirstName() {
+		return studentFirstName;
+	}
+	public void setStudentFirstName(String studentFirstName) {
+		this.studentFirstName = studentFirstName;
+	}
+	public String getStudentLastName() {
+		return studentLastName;
+	}
+	public void setStudentLastName(String studentLastName) {
+		this.studentLastName = studentLastName;
+	}
 
 }
