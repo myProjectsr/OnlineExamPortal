4a3d0175b6076f2a0455d72c8de44638a702a480
rkyash <35574207+rkyash@users.noreply.github.com>
2018-06-26 16:19:26 +0530

    update

--- Exam Management Portal/src/com/rkyash/dao/StudentDao.java
+++ Exam Management Portal/src/com/rkyash/dao/StudentDao.java
@@ -1,5 +1,13 @@
 package com.rkyash.dao;
 
-public class StudentDao {
+import java.util.List;
 
+import com.rkyash.beans.Student;
+
+public interface StudentDao {
+	
+	public void saveOrUpdateUser(Student student);
+	public List<Student> listUser();
+	public Student listUserById(Long studentId);
+	public void deleteUser(Long studentId);
 }
---
+++ Exam Management Portal/src/com/rkyash/dao/StudentDaoImp.java
@@ -0,0 +1,62 @@
+package com.rkyash.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.rkyash.beans.Student;
+
+public class StudentDaoImp implements StudentDao {
+
+	
+	Session session;
+	
+	Transaction tr;
+	
+	@Override
+	public void saveOrUpdateUser(Student student) {
+		try {
+			session.saveOrUpdate(student);
+		} catch (Exception e) {
+			tr.rollback();
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public List<Student> listUser() {
+		List<Student> studentList=null;
+		try {
+			studentList=session.createQuery("form Student").list();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return studentList;
+	}
+
+	@Override
+	public Student listUserById(Long studentId) {
+		Student student=null;
+		try {
+			student=session.get(Student.class, studentId);
+			} catch (Exception e) {
+				e.printStackTrace();
+		}
+		return student;
+	}
+
+	@Override
+	public void deleteUser(Long studentId) {
+		try {
+			Student student=(Student)session.get(Student.class, studentId);
+			session.delete(student);
+		} catch (Exception e) {
+			tr.rollback();
+			e.printStackTrace();
+		}
+		
+	}
+
+}
